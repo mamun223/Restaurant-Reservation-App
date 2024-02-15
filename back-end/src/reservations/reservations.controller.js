02e520418eb52081001d0a237a1317b98cd7f690
Mamood <shabanmamun@gmail.com>
2024-02-15 15:21:31 -0500

    changed the component for the reservations/new route

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,11 +5,11 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const reservations = req.query.reservation_date
+  // const reservations = req.query.reservation_date
 
-  if (reservations) {
-    res.json({ data: await reservationForDate()})
-  }
+  // if (reservations) {
+  //   res.json({ data: await reservationForDate()})
+  // }
   res.json({ data: await service.list() })
 }
 
