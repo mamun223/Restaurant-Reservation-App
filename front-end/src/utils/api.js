770b90eb38c2b05966d260a68a5d49c899c23276
Mamood <shabanmamun@gmail.com>
2024-03-10 06:49:44 -0400

    testing on web

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,8 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-zkhd.onrender.com";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://restaurant-reservation-backend-zkhd.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
@@ -138,7 +139,9 @@ export async function updateReservationStatus(reservationId) {
   const options = {
     method: "PUT",
     headers,
-    body: JSON.stringify({data: { reservation_id: reservationId, status: "booked" } }),
+    body: JSON.stringify({
+      data: { reservation_id: reservationId, status: "booked" },
+    }),
   };
   return await fetchJson(url, options, {});
 }
