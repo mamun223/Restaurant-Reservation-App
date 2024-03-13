6dc478d7a77cb4a790d938b3254d0648b0229189
Mamood <shabanmamun@gmail.com>
2024-03-13 09:29:36 -0400

    search by mobile number works

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,9 @@
 const { PORT = 5001 } = process.env;
 
+const express = require('express');
 const app = require("./app");
 const knex = require("./db/connection");
+const path = require('path');
 
 knex.migrate
   .latest()
@@ -13,6 +15,15 @@ knex.migrate
     console.error(error);
     knex.destroy();
   });
+
+
+  // Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
