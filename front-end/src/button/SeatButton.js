0a533ae5fcc7f170fb9ea04a70dd361c1a22203e
Mamood <shabanmamun@gmail.com>
2024-03-14 01:12:57 -0400

    *****

--- front-end/src/button/SeatButton.js
+++ front-end/src/button/SeatButton.js
@@ -1,9 +1,30 @@
+
+// function SeatButton({ reservation }) {
+//   return (
+//       <button
+//           onClick={() => {
+//               const encodedReservationId = encodeURIComponent(reservation.reservation_id);
+//               window.location.href = `/reservations/${reservation.reservation_id}/seat`;
+//           }}
+//           className="btn"
+//           data-reservation-id-status={reservation.reservation_id}
+//       >
+//           Seat
+//       </button>
+//   );
+// }
+
+
+
+// export default SeatButton;
+
+
 import React from "react";
 import { Link } from "react-router-dom";
 
 function SeatButton({ reservation }) {
   const encodedReservationId = encodeURIComponent(reservation.reservation_id);
-  const seatLink = `/reservations/${encodedReservationId}/seat`;
+  const seatLink = `/reservations/${reservation.reservation_id}/seat`;
 
   return (
     <Link
