ff607c9f475cdbf8a55322f9989e9d0f91c7a28a
Mamood <shabanmamun@gmail.com>
2024-03-14 01:00:31 -0400

    *****

--- front-end/src/button/SeatButton.js
+++ front-end/src/button/SeatButton.js
@@ -1,19 +1,44 @@
 
+// function SeatButton({ reservation }) {
+//   return (
+//       <button
+//           onClick={() => {
+//               const encodedReservationId = encodeURIComponent(reservation.reservation_id);
+//               window.location.href = `/reservations/${encodedReservationId}/seat`;
+//           }}
+//           className="btn"
+//           data-reservation-id-status={reservation.reservation_id}
+//       >
+//           Seat
+//       </button>
+//   );
+// }
+
+
+
+// export default SeatButton;
+
+
+import React from "react";
+import { useHistory } from "react-router-dom";
+
 function SeatButton({ reservation }) {
+  const history = useHistory();
+
+  const handleSeatClick = () => {
+    const encodedReservationId = encodeURIComponent(reservation.reservation_id);
+    history.push(`/reservations/${encodedReservationId}/seat`);
+  };
+
   return (
-      <button
-          onClick={() => {
-              const encodedReservationId = encodeURIComponent(reservation.reservation_id);
-              window.location.href = `/reservations/${encodedReservationId}/seat`;
-          }}
-          className="btn"
-          data-reservation-id-status={reservation.reservation_id}
-      >
-          Seat
-      </button>
+    <button
+      onClick={handleSeatClick}
+      className="btn"
+      data-reservation-id-status={reservation.reservation_id}
+    >
+      Seat
+    </button>
   );
 }
-
-
 
 export default SeatButton;
