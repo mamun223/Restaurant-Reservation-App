e84dd201d8bf746e28383fccf8bb60ce64078433
Mamood <shabanmamun@gmail.com>
2024-03-18 20:06:17 -0400

    *****

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  // process.env.REACT_APP_API_BASE_URL ||
+  process.env.REACT_APP_API_BASE_URL ||
   "https://restaurant-reservation-backend-zkhd.onrender.com";
 
 /**
